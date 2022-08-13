08cfd15bed193c5c872d319aa540d1b5eb487359
Wender Barbosa <wenblack012@gmail.com>
2022-08-13 17:26:46 -0300

    Add start route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express  = require ("express");
 
 const app = express();
 
+
+app.get("/", (request,response)=>{
+  response.send('Hello World');
+});
+
 const PORT = 333;
 
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
