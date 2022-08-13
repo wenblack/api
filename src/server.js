5f26be6052b90024eaab36a2acca24fc817bdc78
Wender Barbosa <wenblack012@gmail.com>
2022-08-13 18:00:55 -0300

    Add subrequest params

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ const express  = require ("express");
 const app = express();
 /*for receive a request use :
 Example:                                                                */
-app.get("/:id", (request,response)=>{
-  response.send(`yor id ${request.params.id}`)
+app.get("/:id/:user", (request,response)=>{
+  
+  const {id , user } = request.params
+  
+  response.send(`
+  Message: ${id}
+  For the user: ${user}
+  `
+  )
 });
 //Root route
 app.get("/", (request,response)=>{
