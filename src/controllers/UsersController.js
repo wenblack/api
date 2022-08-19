4effd03c4bc0965816b3b123c7364a4e62adaca4
Wender Barbosa <wenblack012@gmail.com>
2022-08-19 15:00:47 -0300

    Config App Error to show error message

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,10 +6,15 @@
    update - put method to update a register
    delete - delete method to exclude a  register
  */
-
+const AppError = require ("../utils/AppError")
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if(!name){
+      throw new AppError("Name is mandatory")
+    }
+
     response.status(201).json({name, email, password});
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+//Import Async Errors
+require("express-async-errors");
+const AppError = require("./utils/AppError");
 //import Express
 const express = require("express");
 //Routes of application:
@@ -6,7 +9,20 @@ const routes = require("./routes");
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next)=>{
+  if( error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+  console.error(error);
 
+  return response.status(500).json({
+    status:'error',
+    message: 'Internal Server Error',
+  })
+})
 /* Request Example:                                                                
 app.get("/:id/:user", (request,response)=>{
   
