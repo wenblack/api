1d68731fa98bc9506877817413982133037fda91
Wender Barbosa <wenblack012@gmail.com>
2022-09-04 11:12:32 -0300

    Create Controller and Routes for Tags

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -3,18 +3,7 @@ const { Router, request } = require("express");
 const NotesController = require("../controllers/NotesController");
 
 const notesRoutes = Router();
-/*Middleware Example
-function myFirsMiddleware(result, response, next) {
-  console.log("This is the return of middleware!");
 
-  if(!request.body.isAdmin){
-    return response.json({ message: "User unauthorized"});
-  } 
-  
-  next();
-
-}
-*/
 
 const notesController = new NotesController();
 //Query params 
