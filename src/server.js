a3c689a2cf9a25dcdd52d0ece558b2b8cb4eafe7
wenblack <wenblack012@gmail.com>
2023-04-15 04:44:39 -0300

    ✅ Altered port to production ✅

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.use(( error, request, response, next ) => {
     });
 });
 
-const PORT = 3333;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
