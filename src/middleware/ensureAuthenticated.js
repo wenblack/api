0ddd5ca795df314a94bfa88b1d1acedfdf5cf1ad
wenblack <wenblack012@gmail.com>
2023-04-15 06:17:05 -0300

    ✅ Altered jwt Logic  ✅

--- src/middleware/ensureAuthenticated.js
+++ src/middleware/ensureAuthenticated.js
@@ -3,13 +3,11 @@ const AppError = require("../utils/AppError");
 const authConfig = require("../configs/auth");
 
 function ensureAuthenticated(request, response, next) {
-    const authHeader = request.headers.authorization;
+    const token = request.headers.authorization;
 
     if(!authHeader) {
         throw new AppError("JWT Token não informado", 401);
     }
-
-    const [, token] = authHeader.split(" ");
 
     try {
         const { sub: user_id } = verify(token, authConfig.jwt.secret);
